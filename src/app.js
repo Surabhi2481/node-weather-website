35273c0424ac1bc568651f2047af1e0ad113804e
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-11-11 20:10:02 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,138 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const { title } = require('process')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+// app.get('', (req, res) => {
+//     res.send('<h1>Hello Express!</h1>')
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'Surabhi',
+//         age: 22
+//     })
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About Page!</h1>')
+// })
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App!',
+        name: 'Surabhi'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Surabhi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'Surabhi',
+        email: 'surabhi@gmail.com'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if(error){
+            return res.send({error})
+        }
+
+        forecast(longitude, latitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+    
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
+        // forecast(latitude, longitude, (error, forecastData) => {
+        //     if(error){
+        //         return res.send({error})
+        //     }
+        // })
+
+        // res.send({
+        //     forecast: forecastData,
+        //     location,
+        //     address: req.query.address
+        // })
+    })
+    // console.log(req.query.address)
+    // res.send({
+    //     forecast: 'It is snowing!',
+    //     location: 'Philadelphia',
+    //     address: req.query.address
+    // })
+
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404',  {
+        title: 404,
+        name: 'Surabhi',
+        errorMessage: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 404,
+        name: 'Surabhi',
+        errorMessage: 'Page not found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
