5a52a47bd96e615a16766b7131c9e390eddc2305
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-11-11 22:44:00 +0530

    added humidity part

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location!', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature +' degrees out. There is a '+ body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature +' degrees out. There is a '+ body.current.precip + '% chance of rain. Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
